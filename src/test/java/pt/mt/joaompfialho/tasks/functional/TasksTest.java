f6393d79109bddc1d008896ce85cd3189f25e213
João Fialho <joao.fialho@hexis-hub.com>
2020-04-26 10:31:27 +0100

    fix ip address

--- src/test/java/pt/mt/joaompfialho/tasks/functional/TasksTest.java
+++ src/test/java/pt/mt/joaompfialho/tasks/functional/TasksTest.java
@@ -18,8 +18,8 @@ public class TasksTest {
 	public WebDriver acessarAplicacao() throws MalformedURLException {
 		//WebDriver driver = new ChromeDriver();
 		DesiredCapabilities cap = DesiredCapabilities.chrome();
-		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.4:4444/wd/hub"), cap);
-		driver.navigate().to("http://192.168.1.4:8001/tasks");
+		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.70:4444/wd/hub"), cap);
+		driver.navigate().to("http://192.168.1.70:8001/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
 	}
